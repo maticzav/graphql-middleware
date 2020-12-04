cae08875998423ec44c8b617b04b62d1e4563d79
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2020-12-03 21:58:30 -0300

    test(integration): remove graphql yoga

--- tests/integration.test.ts
+++ tests/integration.test.ts
@@ -6,58 +6,6 @@ import { AddressInfo } from 'ws'
 import { applyMiddleware } from '../src'
 
 describe('integrations', () => {
-  test('GraphQL Yoga', async () => {
-    const typeDefs = `
-      type Query {
-        test: String!
-      }
-    `
-
-    const resolvers = {
-      Query: {
-        test: () => 'test',
-      },
-    }
-
-    const schema = makeExecutableSchema({ typeDefs, resolvers })
-
-    const schemaWithMiddleware = applyMiddleware(schema, async resolve => {
-      const res = await resolve()
-      return `pass-${res}`
-    })
-
-    const server = new YogaServer({
-      schema: schemaWithMiddleware,
-    })
-
-    const http = await server.start({ port: 0 })
-    const { port } = http.address() as AddressInfo
-    const uri = `http://localhost:${port}/`
-
-    /* Tests */
-
-    const query = `
-      query {
-        test
-      }
-    `
-
-    const body = await request({
-      uri,
-      method: 'POST',
-      json: true,
-      body: { query },
-    }).promise()
-
-    /* Tests. */
-
-    expect(body).toEqual({
-      data: {
-        test: 'pass-test',
-      },
-    })
-  })
-
   test('ApolloServer', async () => {
     /* Schema. */
     const typeDefs = `
